12574b0225f91d66d970e1cc4e9e22a27d9fa77f
Chris Caudill <christopherscaudill@gmail.com>
2019-01-29 20:24:34 -0500

    cshw1

--- CS-Homework1/Program.cs
+++ CS-Homework1/Program.cs
@@ -10,6 +10,32 @@ namespace CS_Homework1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Program start");
+            WhileCount();
+            WhileNumber();
+
+        }
+        static void WhileCount()
+        {
+            var counter = 0;
+            while (counter < 101)
+            {
+                Console.WriteLine($"Number {counter}");
+                counter = counter +1;
+            }
+        }
+        static void WhileNumber()
+        {
+            var max = 25;
+            var number = 1;
+            var increment = 1;
+            while (number < max)
+            {
+                Console.WriteLine($"Other Number is {number}");
+                //Console.WriteLine($"The count is {increment}");
+                number = number * increment;
+                increment++;
+            }
         }
     }
 }
